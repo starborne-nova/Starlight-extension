c301d4e0c9749bb1323c71c0df4951f4275534ad
Billy <sirwilliam78@gmail.com>
2021-09-02 19:23:54 -0400

    Initial Commit

---
+++ background.js
@@ -0,0 +1,80 @@
+const url = "https://jabroni-server.herokuapp.com/pulse";
+
+var streamerID;
+
+browserAction.setBadgeBackgroundColor("#0a1f27");
+browserAction.setBadgeText("7357");
+
+chrome.alarms.create("twitchPulse", {
+    delayInMinutes: 5,      //5 Min for Dev, 1 Min for Deploy
+    periodInMinutes: 5
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    if (alarm.name === "twitchPulse") {
+        pulse();
+    }
+});
+
+function pulse() {
+    fetch(
+        url,
+        {
+            method: "post",
+            headers:
+            {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            body: { "streamer": "Jabroni Mike" }
+        }
+    )
+        .then(response => {
+            console.log(response)
+
+            if (response.body === true) {
+                chrome.storage.sync.set({jabroniLive: true}, function() {
+                    console.log("Mike is currently online");
+                  });                      
+            }
+            if (response.body === false) {
+                chrome.storage.sync.set({jabroniLive: false}, function() {
+                    console.log("Mike is currently offline");
+                  });
+            }
+            else {
+                console.log("Unknown Response Recieved")
+            }
+
+        }
+        )
+        fetch(
+            url,
+            {
+                method: "post",
+                headers:
+                {
+                    "Content-type": "application/json; charset=UTF-8"
+                },
+                body: { "streamer": "TEST" }
+            }
+        )
+            .then(response => {
+                console.log(response)
+    
+                if (response.body === true) {
+                    chrome.storage.sync.set({testDev: true}, function() {
+                        console.log("TEST is currently online");
+                      });                      
+                }
+                if (response.body === false) {
+                    chrome.storage.sync.set({testDev: false}, function() {
+                        console.log("TEST is currently offline");
+                      });
+                }
+                else {
+                    console.log("Unknown Response Recieved")
+                }
+    
+            }
+            )
+}
