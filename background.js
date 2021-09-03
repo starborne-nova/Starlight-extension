e1172d9bb28cafcd134cdf1c35af82adf6ad3c05
Billy <sirwilliam78@gmail.com>
2021-09-03 15:59:09 -0400

    Badge updates

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 const url = "https://jabroni-server.herokuapp.com/pulse";
 
 var streamerID;
+var badgeNumber = 0;
 
 chrome.action.setBadgeBackgroundColor({ color: "#0a1f27" }, function () { console.log("background color changed") });
-chrome.action.setBadgeText({ text: "123" }, function () { console.log("badge text changed") });
+chrome.action.setBadgeText({ text: "1" }, function () { console.log("badge text changed") });
 
 chrome.alarms.create("twitchPulse", {
     delayInMinutes: 1,
@@ -36,11 +37,22 @@ function pulse() {
             if (data === true) {
                 chrome.storage.sync.set({ jabroniLive: true }, function () {
                     console.log("Mike is currently online");
+                    badgeNumber++;
+                    chrome.action.setBadgeText({ text: "1" }, function () { console.log("badge text changed") });
+
                 });
             }
             if (data === false) {
                 chrome.storage.sync.set({ jabroniLive: false }, function () {
                     console.log("Mike is currently offline");
+                    if (chrome.storage.sync.get(["testDev"], function (result) { return result.testDev }) === false) {
+                        badgeNumber = 0;
+                        chrome.action.setBadgeText({ text: "0" }, function () { console.log("badge text changed") });
+                    }
+                    if (chrome.storage.sync.get(["testDev"], function (result) { return result.testDev }) === true) {
+                        badgeNumber = 1;
+                        chrome.action.setBadgeText({ text: "1" }, function () { console.log("badge text changed") });
+                    }
                 });
             }
             else {
@@ -66,11 +78,26 @@ function pulse() {
                 if (data === true) {
                     chrome.storage.sync.set({ testDev: true }, function () {
                         console.log("TEST is currently online");
+                        badgeNumber++;
+                        if (badgeNumber === 0) {
+                            chrome.action.setBadgeText({ text: "1" }, function () { console.log("badge text changed") });
+                        }
+                        if (badgeNumber === 1) {
+                            chrome.action.setBadgeText({ text: "2" }, function () { console.log("badge text changed") });
+                        }
                     });
                 }
                 if (data === false) {
                     chrome.storage.sync.set({ testDev: false }, function () {
                         console.log("TEST is currently offline");
+                        if (chrome.storage.sync.get(["jabroniLive"], function (result) { return result.testDev }) === false) {
+                            badgeNumber = 0;
+                            chrome.action.setBadgeText({ text: "0" }, function () { console.log("badge text changed") });
+                        }
+                        if (chrome.storage.sync.get(["jabroniLive"], function (result) { return result.testDev }) === true) {
+                            badgeNumber = 1;
+                            chrome.action.setBadgeText({ text: "1" }, function () { console.log("badge text changed") });
+                        }
                     });
                 }
                 else {
--- popup.js
+++ popup.js
@@ -1,27 +1,27 @@
 const options = {}
 
 chrome.storage.sync.get('options', (data) => {
-    Object.assign(options, data.options);
-  });
+  Object.assign(options, data.options);
+});
 
-  chrome.storage.onChanged.addListener(function (changes, namespace) {
-    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-      if(key === jabroniLive && newValue === true){
-        document.getElementById("jabroniMike").innerHTML = "Jabroni Mike: LIVE";
-      }
-      if(key === jabroniLive && newValue === false){
-        document.getElementById("jabroniMike").innerHTML = "Jabroni Mike: OFFLINE";
-      }
-      if(key === testDev && newValue === true){
-        document.getElementById("testDev").innerHTML = "TEST: LIVE";
-      }
-      if(key === testDev && newValue === false){
-        document.getElementById("testDev").innerHTML = "TEST: OFFLINE";
-      }
-      
-      console.log(
-        `Storage key "${key}" in namespace "${namespace}" changed.`,
-        `Old value was "${oldValue}", new value is "${newValue}".`
-      );
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    if (key === jabroniLive && newValue === true) {
+      document.getElementById("mikeStatus").className = "online";
     }
-  });
+    if (key === jabroniLive && newValue === false) {
+      document.getElementById("jabroniMike").className = "offline";
+    }
+    if (key === testDev && newValue === true) {
+      document.getElementById("testStatus").className = "online";
+    }
+    if (key === testDev && newValue === false) {
+      document.getElementById("testStatus").className = "offline";
+    }
+
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+  }
+});
